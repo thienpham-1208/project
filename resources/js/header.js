c8854afb7518d1ed4b801d557f9389c5deec52bd
thienpham-0812 <thienpham-0812@example.com>
2021-09-17 13:02:20 +0700

    design-base-frontend

--- resources/js/header.js
+++ resources/js/header.js
@@ -1,2 +1,22 @@
+window.Swiper = require('./libs/swiper/swiper-bundle.min.js');
+require('./libs/swiper/swiper-bundle.min.css');
+
 var date = new Date();
 stringDayTime(date, 'date', 'time');
+
+var mySwiper = new Swiper('.swiper-container', {
+    direction: 'horizontal',
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+
+});
+setTimeout(() => {
+    mySwiper;
+}, 500);
